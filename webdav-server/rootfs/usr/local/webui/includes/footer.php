ae409fcf1339968f9a6b66dc83d6d04518f92a82
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-30 00:23:01 +0200

    Create footer.php

---
+++ webdav-server/rootfs/usr/local/webui/includes/footer.php
@@ -0,0 +1,11 @@
+    <footer class="bg-dark text-white text-center py-3 mt-5">
+        <div class="container">
+            <p class="mb-0">&copy; <?php echo date('Y'); ?> WebDAV Explorer Add-on</p>
+        </div>
+    </footer>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
